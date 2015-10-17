90f190057e40d3904f83bf6d8e0e8133380067a9
Sarah Le Cornec <sarahlecornec@Sarahs-MBP.lan>
2015-10-17 18:29:13 +0100

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | The Hobbit Gallery"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | The Helpful Help Page, Featuring Bilbo Baggins"
   end
+  
+  test "should get about" do
+  	get :about
+  	assert_response :success
+  	assert_select "title", "About | Bilbo Baggins? More Like Bilbo Swaggins"
+  end 
 
 end
